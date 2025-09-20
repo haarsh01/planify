34d46055cf04c289fb268dd2b9a0a30f4afcdadf
Harsh <harshkaushik717@gmail.com>
2025-09-20 12:30:08 -0300

    Added User Authentication

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthLayout = ({children}) => {
+
+  return (
+    <div className="flex justify-center pt-20">
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout;
---
+++ components/header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from './ui/button';
+import { PenBox } from 'lucide-react';
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+
+import UserMenu from './user-menu';
+
+const Header = () => {
+  return (
+    <nav className="mx-auto py-2 px-4 flex justify-between">
+<Link href={"/"} className="flex items-center">
+<Image
+src="/logo.png"
+width="150"
+height="60"
+alt="Logo"
+className="h 16 w-auto"
+/>
+
+</Link>
+<div className="flex items-center gap-4">
+<Link href="/events?create=true">
+          <Button variant="default" className="flex items-center gap-2">
+            <PenBox size={18} />
+            <span className="hidden sm:inline">Create Event</span>
+          </Button>
+        </Link>
+        <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+            </SignInButton>
+        </SignedOut>
+        <SignedIn>
+            <UserMenu />
+        </SignedIn>
+
+</div>
+
+    </nav>
+  
+  )
+}
+
+export default Header
