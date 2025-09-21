ee9e7d3275c8a1ecc5a593cc331cdddc9a59605a
Harsh <harshkaushik717@gmail.com>
2025-09-21 17:07:14 -0300

    Added Internal Tabs and worked on errors

--- middleware.js
+++ middleware.js
@@ -1,18 +1,8 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-const isProtectedRoute = createRouteMatcher([
-    "/dashboard(.*)",
-    "/events(.*)",
-    "/meetings(.*)",
-    "/availability(.*)",
-  ]);
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-  export default clerkMiddleware((auth, req) => {
-    if (!auth().userId && isProtectedRoute(req)) {
-      // Add custom logic to run before redirecting
-  
-      return auth().redirectToSignIn();
-    }
-  });
+export default clerkMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up"]
+});
 
 export const config = {
   matcher: [
