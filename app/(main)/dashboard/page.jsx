2f3c788427c0fafbd1384935ec1fabae2bb5c7b1
Harsh <harshkaushik717@gmail.com>
2025-09-22 07:51:07 -0300

    Added Dashboard Features

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,70 @@
+"use client";
 import React from 'react'
+import {useUser} from "@clerk/nextjs";
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+import { Input } from "@/components/ui/input";
+import {useForm} from "react-hook-form";
+import { usernameSchema } from '@/app/lib/validators';
+import {zodResolver} from "@hookform/resolvers/zod"
+import { useEffect } from 'react';
 
-const page = () => {
+
+
+const Dashboard = () => {
+  const {isLoaded, user} = useUser();
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(usernameSchema),
+  });
+
+  useEffect(() =>{
+    setValue("username", user?.username);
+
+  }, [isLoaded]);
+
+
+
+  const onSubmit = async (data) => {};
+
   return (
     <div>
-      Dashboard
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Welcome, {user ?.firstName}
+          </CardTitle>
+        </CardHeader>
+
+      </Card>
+
+      <CardHeader>
+        <CardTitle>
+          Your Unique Link
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}className="space-y-4">
+          <div>
+            <div className="flex items-center gap-2">
+              <span>{window?.location.origin}</span>
+              <Input{...register("username")} placeholder="username" />
+            </div>
+{errors.username && (
+
+  <p className="text-red-500 text-sm mt-1">{errors.username.message}</p>
+)}
+
+          </div>
+          <Button type="submit">Update Username</Button>
+        </form>
+      </CardContent>
     </div>
   )
 }
 
-export default page
+export default Dashboard
